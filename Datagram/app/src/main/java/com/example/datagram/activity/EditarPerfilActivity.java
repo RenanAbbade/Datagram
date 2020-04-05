9fd0ef422db7db253ce15dfb681b3e86259c13fa
Rafael Silva Lima <rsillima@everis.com>
2020-04-05 16:25:45 -0300

    alterando imagem do perfil

--- Datagram/app/src/main/java/com/example/datagram/activity/EditarPerfilActivity.java
+++ Datagram/app/src/main/java/com/example/datagram/activity/EditarPerfilActivity.java
@@ -1,6 +1,11 @@
 package com.example.datagram.activity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,9 +16,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.datagram.R;
+import com.example.datagram.helper.ConfiguracaoFirebase;
 import com.example.datagram.helper.UsuarioFirebase;
 import com.example.datagram.model.Usuario;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -24,6 +37,8 @@ public class EditarPerfilActivity extends AppCompatActivity {
     private TextInputEditText editNomePerfil, editEmailPerfil;
     private Button buttonSalvarAlteracoes;
     private Usuario usuarioLogado;
+    private static final int SELECAO_GALERIA = 200;
+    private StorageReference storageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +46,8 @@ public class EditarPerfilActivity extends AppCompatActivity {
         setContentView(R.layout.activity_editar_perfil);
 
         //configuracoes iniciais
-        //aqui ja teremos o usuario com seus dados, nome, email, photo...
         usuarioLogado = UsuarioFirebase.getDadosUsuarioLogado();
+        storageRef = ConfiguracaoFirebase.getFirebaseStorage();
 
         //Configura Toolbar
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
@@ -68,6 +83,70 @@ public class EditarPerfilActivity extends AppCompatActivity {
 
             }
         });
+
+        textAlterarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                if(i.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(i,SELECAO_GALERIA);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            Bitmap imagem = null;
+
+            try{
+                switch (requestCode){
+                    case SELECAO_GALERIA:
+                        Uri localimagemSelecionada = data.getData();
+                        imagem = MediaStore.Images.Media.getBitmap(getContentResolver(),localimagemSelecionada);
+                         break;
+                }
+
+                //caso user tenha escolhido uma imagem
+                if(imagem != null){
+                    //configurar imagem na tela
+                    imageEditarPerfil.setImageBitmap(imagem);
+
+                    //recuperar dados da img para o firebase
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    imagem.compress(Bitmap.CompressFormat.JPEG,70,baos);
+                    byte [] dadosImagem = baos.toByteArray();
+
+                    //salvar imagem no firebase
+                    StorageReference imagemRef = storageRef
+                            .child("imagens")
+                            .child("perfil")
+                            .child("<id-usuario>.jpeg");
+                    UploadTask uploadTask = imagemRef.putBytes(dadosImagem);
+                    uploadTask.addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(EditarPerfilActivity.this,
+                                    "erro ao fazer upload da imagem",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(EditarPerfilActivity.this,
+                                    "Sucesso ao fazer upload da imagem",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    ;
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
     public void inicializarComponentes(){
