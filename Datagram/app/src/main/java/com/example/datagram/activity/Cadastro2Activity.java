85d507b5e732dc6cff66968dd2c8b82886ff1fe8
Renan Henrique da Silva Abbade <36777338+RenanAbbade@users.noreply.github.com>
2020-04-22 20:18:46 -0300

    Input de data
    Criação de input da data

--- Datagram/app/src/main/java/com/example/datagram/activity/Cadastro2Activity.java
+++ Datagram/app/src/main/java/com/example/datagram/activity/Cadastro2Activity.java
@@ -1,11 +1,15 @@
 package com.example.datagram.activity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
@@ -27,6 +31,10 @@ import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 import com.google.firebase.database.DatabaseReference;
 
+import org.w3c.dom.CDATASection;
+
+import java.util.Calendar;
+
 
 public class Cadastro2Activity extends AppCompatActivity {
 
@@ -39,6 +47,8 @@ public class Cadastro2Activity extends AppCompatActivity {
     private Usuario usuario;
     private DatabaseReference usuariosRef;
     private String tipoUsuario = "Membro";
+    public  EditText campoData;
+    DatePickerDialog.OnDateSetListener setListener;
     //Obj autenticacao
     private FirebaseAuth autenticacao;
 
@@ -286,6 +296,40 @@ public class Cadastro2Activity extends AppCompatActivity {
             progressBar02 = findViewById(R.id.progressBar02);
 
             campoNome.requestFocus();
+        }
+
+        public void validaData(View v){
+            campoData = campoDataNasc;
+
+            if(campoDataInicioTrabalho.isPressed()){
+                campoData = campoDataInicioTrabalho;
+            }
+
+            Calendar calendar = Calendar.getInstance();
+
+            final int year = calendar.get(Calendar.YEAR);
+            final int month = calendar.get(Calendar.MONTH);
+            final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+            campoData.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    DatePickerDialog datePickerDialog = new DatePickerDialog(
+                            Cadastro2Activity.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,setListener, year,month,day);
+                    datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                    datePickerDialog.show();
+                }
+            });
+
+
+            setListener = new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int day) {
+                    month = month +1;
+                    String date = day+"/"+month+"/"+year;
+                    campoData.setText(date);
+                }
+            };
 
         }
 
