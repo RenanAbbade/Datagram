5eb5aca0fe3253b59f7a5fedfe15c08c75565dd6
Renan Henrique da Silva Abbade <36777338+RenanAbbade@users.noreply.github.com>
2020-05-27 16:11:06 -0300

    Correção arquivo .git

---
+++ Datagram WEB/Datagram-frontend/src/app/services/login-service.service.ts
@@ -0,0 +1,29 @@
+import { AppConstants } from './../app-constants';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import {Router} from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginServiceService {
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  public login(usuario){
+
+    console.log(JSON.stringify(usuario));
+
+
+    return this.http.post(AppConstants.baseLoginInsert, usuario, {responseType: 'text'}).subscribe(data => {
+     console.log(data);
+    // Implementar auth jwt spring security
+     if (data === 'auth'){
+      this.router.navigate(['home']);
+     }else{
+       alert('Credenciais não encontradas!');
+     }
+    });
+    }
+  }
+
