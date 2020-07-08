236a5cd9d0d7843da1efc59f381ecc8ea528bda4
Renan Henrique da Silva Abbade <36777338+RenanAbbade@users.noreply.github.com>
2020-07-07 23:48:43 -0300

    Correção no download de pdf por terceiros

--- Datagram WEB/Datagram-frontend/src/app/feed/feed.component.ts
+++ Datagram WEB/Datagram-frontend/src/app/feed/feed.component.ts
@@ -60,8 +60,47 @@ export class FeedComponent implements OnInit {
     });
   }
 
-  public mandaPdf(base64){
-    this.postService.mandaPdf(base64);
+  public mandaPdf(data64){
+    var type = 'application/pdf';
+    let blob = null;
+    const blobURL = URL.createObjectURL( this.pdfBlobConversion(data64, 'application/pdf'));
+    const theWindow = window.open(blobURL);
+    const theDoc = theWindow.document;
+    const theScript = document.createElement('script');
+
+    function injectThis() {
+            window.print();
+    }
+
+    theScript.innerHTML = 'window.onload = ${injectThis.toString()};';
+    theDoc.body.appendChild(theScript);
+
+  }
+
+  //converts base64 to blob type for windows
+  public pdfBlobConversion(b64Data, contentType) {
+    contentType = contentType || '';
+    var sliceSize = 512;
+    b64Data = b64Data.replace(/^[^,]+,/, '');
+    b64Data = b64Data.replace(/\s/g, '');
+    var byteCharacters = window.atob(b64Data);
+    var byteArrays = [];
+
+    for ( var offset = 0; offset < byteCharacters.length; offset = offset + sliceSize ) {
+      var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+      var byteNumbers = new Array(slice.length);
+      for (var i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+
+      var byteArray = new Uint8Array(byteNumbers);
+
+      byteArrays.push(byteArray);
+    }
+
+    var blob = new Blob(byteArrays, { type: contentType });
+    return blob;
   }
 
   ngOnInit(): void {
